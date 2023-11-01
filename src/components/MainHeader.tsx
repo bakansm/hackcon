20a099407c40ec2ea2adc2bf795c321c7d42d7c6
bakansm <khuynhbao5@gmail.com>
2023-11-01 21:53:59 +0700

    style: change header style

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -1,64 +1,94 @@
 import { useSignInRedirect } from '@/hooks/useSignInRedirect';
 import { useAuthStore } from '@/stores/auth';
-import Image from 'next/image';
-import Link from 'next/link';
-import styled from 'styled-components';
-import LogoBlack from '@/assets/images/near-logo-black.svg';
 import { Text } from './lib/Text';
-
-const Header = styled.header`
-  background: #fff;
-  display: flex;
-  align-items: center;
-  padding: 1rem;
-  gap: 1rem;
-
-  button {
-    background: #000;
-    color: #fff;
-    padding: 0.3rem 1rem;
-    border-radius: 10rem;
-    cursor: pointer;
-  }
-`;
-
-const Logo = styled.div`
-  margin-right: auto;
-`;
+import {
+	Box,
+	Button,
+	ButtonGroup,
+	Center,
+	Container,
+	Flex,
+	Heading,
+} from '@chakra-ui/react';
+import { useRouter } from 'next/router';
 
 export const MainHeader = () => {
-  const signedIn = useAuthStore((store) => store.signedIn);
-  const accountId = useAuthStore((store) => store.accountId);
-  const logOut = useAuthStore((store) => store.logOut);
-  const { requestAuthentication } = useSignInRedirect();
+	const signedIn = useAuthStore((store) => store.signedIn);
+	const accountId = useAuthStore((store) => store.accountId);
+	const logOut = useAuthStore((store) => store.logOut);
+	const { requestAuthentication } = useSignInRedirect();
+	const router = useRouter();
 
-  return (
-    <Header>
-      <Logo>
-        <Link href="/">
-          <Image src={LogoBlack} alt="NEAR" />
-        </Link>
-      </Logo>
+	return (
+		<Center height={'84px'}>
+			<Container
+				maxW={'1440px'}
+				display={'flex'}
+				alignItems={'center'}
+				justifyContent={'space-between'}
+			>
+				<Box
+					display={'flex'}
+					gap={'2rem'}
+				>
+					<Heading textTransform={'uppercase'}>hackcon</Heading>
 
-      <Link href="/">Home</Link>
+					<Flex gap={'1rem'}>
+						<Button
+							variant={'link'}
+							onClick={() => router.push('/')}
+						>
+							Home
+						</Button>
+						<Button
+							variant={'link'}
+							onClick={() => router.push('/')}
+						>
+							Blog
+						</Button>
+						<Button
+							variant={'link'}
+							onClick={() => router.push('/')}
+						>
+							Event
+						</Button>
+						<Button
+							variant={'link'}
+							onClick={() => router.push('/hackathon')}
+						>
+							Hackathon
+						</Button>
+					</Flex>
+				</Box>
 
-      {signedIn ? (
-        <>
-          <Text>{accountId}</Text>
-          <button type="button" onClick={logOut}>
-            Log Out
-          </button>
-        </>
-      ) : (
-        <>
-          <button type="button" onClick={() => requestAuthentication()}>
-            Sign In
-          </button>
-          <button type="button" onClick={() => requestAuthentication(true)}>
-            Create Account
-          </button>
-        </>
-      )}
-    </Header>
-  );
+				{signedIn ? (
+					<>
+						<Text>{accountId}</Text>
+						<button
+							type='button'
+							onClick={logOut}
+						>
+							Log Out
+						</button>
+					</>
+				) : (
+					<ButtonGroup gap={'1'}>
+						<Button
+							type='button'
+							onClick={() => requestAuthentication(true)}
+						>
+							Create Account
+						</Button>
+						<Button
+							type='button'
+							colorScheme='teal'
+							onClick={() => requestAuthentication()}
+						>
+							Sign In
+						</Button>
+					</ButtonGroup>
+				)}
+			</Container>
+		</Center>
+	);
 };
